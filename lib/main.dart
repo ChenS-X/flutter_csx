12f4a0cccc6acfe0a968dc240c3cdf3b100abe62
ChenS-X <chensxyouxiang@163.com>
2020-07-06 06:48:32 +0800

    设置标题栏左右按钮(leading&actions)

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,45 @@
 import 'package:flutter/material.dart';
 
-import 'modle/post.dart';
+import './demo/listView_demo.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Home(),
-      theme: ThemeData(
-        primarySwatch: Colors.yellow
-      ),
+      debugShowCheckedModeBanner: false, //隐藏右上角的debug条
+      theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
 }
 
-class Home extends StatelessWidget{
-
-  Widget _listItemBuilder(BuildContext context,int index){
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child:Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl,fit:BoxFit.cover,),
-          SizedBox(height: 16.0,),
-          Text(
-            posts[index].title,
-            style:Theme.of(context).textTheme.bodyText1
-          ),
-          Text(
-            posts[index].author,
-            style:Theme.of(context).textTheme.subtitle1
-          ),
-          SizedBox(height: 16.0,)
-        ],
-      )
-    );
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.grey[100],
-        appBar:AppBar(
-          title: Text("CHENSX"),
-          elevation: 0.0,//设置标题栏下部阴影
-          centerTitle: true,//设置标题位置
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: "navigation button",
+          onPressed: () => debugPrint('navigation button is pressed'),
         ),
-        body: ListView.builder(
-          itemCount:posts.length,
-          itemBuilder:_listItemBuilder
-        ),
-      );
-  }
-}
-
-class Hello extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child:Text(
-        "Hello",
-        textDirection:TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.black87,
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold
-        ),
-      )
+        title: Text("CHENSX"),
+        elevation: 0.0, //设置标题栏下部阴影
+        centerTitle: true, //设置标题位置
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "search button",
+            onPressed: () => debugPrint('Search button is pressed'),
+          ),
+        ],
+      ),
+      body: null,
     );
   }
 }
