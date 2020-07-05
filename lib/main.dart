f37a9aa737a0e39115a8c0d3b1efd591766a2e26
ChenS-X <chensxyouxiang@163.com>
2020-07-06 00:24:10 +0800

    使用materialapp组件

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,32 @@ class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+          title: Text("CHENSX"),
+          elevation: 0.0,//设置标题栏下部阴影
+          centerTitle: true,//设置标题位置
+        ),
+        body: Hello(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Center(
       child:Text(
         "Hello",
         textDirection:TextDirection.ltr,
         style: TextStyle(
-          color: Colors.yellow,
+          color: Colors.black87,
           fontSize: 40.0,
           fontWeight: FontWeight.bold
         ),
