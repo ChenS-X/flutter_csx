31df135a96c1545c771b28083f1eb1148db78ec6
ChenS-X <chensxyouxiang@163.com>
2020-07-06 00:10:23 +0800

    自定义小部件

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    Center(
+void main() => runApp(App());
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
       child:Text(
         "Hello",
-        textDirection:TextDirection.ltr
+        textDirection:TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.yellow
+        ),
       )
-    )
-  );
+    );
+  }
 }
