10b95e9a560ac2010ec562a63ba8e90e1e4c79d5
ChenS-X <chensxyouxiang@163.com>
2020-07-06 01:00:23 +0800

    新增post数据，使用listview.builder创建列表项目

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'modle/post.dart';
+
 void main() => runApp(App());
 
 class App extends StatelessWidget{
@@ -7,18 +9,53 @@ class App extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
+      home: Home(),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget{
+
+  Widget _listItemBuilder(BuildContext context,int index){
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child:Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0,),
+          Text(
+            posts[index].title,
+            style:Theme.of(context).textTheme.bodyText1
+          ),
+          Text(
+            posts[index].author,
+            style:Theme.of(context).textTheme.subtitle1
+          ),
+          SizedBox(height: 16.0,)
+        ],
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
         appBar:AppBar(
           title: Text("CHENSX"),
           elevation: 0.0,//设置标题栏下部阴影
           centerTitle: true,//设置标题位置
         ),
-        body: Hello(),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.yellow
-      ),
-    );
+        body: ListView.builder(
+          itemCount:posts.length,
+          itemBuilder:_listItemBuilder
+        ),
+      );
   }
 }
 
---
+++ lib/modle/post.dart
@@ -0,0 +1,49 @@
+class Post {
+  const Post({this.title, this.author, this.imageUrl});
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+      author: '匿名',
+      title: '好玩的卡通电脑桌面壁纸',
+      imageUrl:
+          'http://pic1.win4000.com/wallpaper/d/525e00294ac25.jpg'),
+  Post(
+      author: '匿名1',
+      title: '动漫 卡通',
+      imageUrl:
+          'http://5b0988e595225.cdn.sohucs.com/images/20170915/f9c4f831048844dca4e98675180095b4.jpeg'),
+  Post(
+      author: '匿名2',
+      title: '『割绳子』游戏主题壁纸',
+      imageUrl:
+          'http://attach.bbs.miui.com/forum/201308/11/120728a3237qmw2c1k2y3k.jpg'),
+  Post(
+      author: '匿名3',
+      title: '纸狗狗',
+      imageUrl:
+          'http://5b0988e595225.cdn.sohucs.com/q_70,c_zoom,w_640/images/20170819/910d2f57a7e44c9fbecdbafc56382a62.jpeg'),
+  Post(
+      author: '二哈猫',
+      title: '可爱的二哈猫',
+      imageUrl:
+          'http://desk.fd.zol-img.com.cn/t_s960x600c5/g5/M00/06/07/ChMkJlc1jruIN6qXAAZiH7pZdhkAARYIADw6pwABmI3936.jpg'),
+  Post(
+      author: '困了的',
+      title: '纽约之夜',
+      imageUrl:
+          'http://hbimg.b0.upaiyun.com/aabcdd83afa8d4ec5e67b92304448cc65f4c5a571fbbe-OJagmR_fw658'),
+  Post(
+      author: '其斧鱼',
+      title: '可爱的恐龙怪兽',
+      imageUrl:
+          'http://hbimg.b0.upaiyun.com/90cf75679b823d194800e8543a3a378a872a9954468ce-CRiTWM_fw658'),
+  Post(
+      author: '尼克斯及zz',
+      title: '流氓兔的旅行',
+      imageUrl: 'http://pic1.win4000.com/wallpaper/d/525e00640a4fd.jpg'),
+];
