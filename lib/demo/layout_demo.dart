e8dfc3a3c4b6afaecdca6b53b4ad6bafcaabd958
ChenS-X <chensxyouxiang@163.com>
2020-07-09 07:00:46 +0800

    使用constrainedBox组件

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -8,11 +8,9 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          AspectRatio(
-            // aspectRatio: 2.0/1.0,
-            // aspectRatio: 1.0/1.0, // 和aspectRatio:1 效果一致
-            // aspectRatio: 1.4,
-            aspectRatio: 16.0/9.0,
+          ConstrainedBox(
+            //带限制的组件
+            constraints: BoxConstraints(minHeight: 200.0, maxWidth: 200.0),
             child: Container(
               color: Color.fromRGBO(3, 54, 255, 1.0),
             ),
@@ -23,110 +21,123 @@ class LayoutDemo extends StatelessWidget {
   }
 }
 
-
-class StackDemo extends StatelessWidget{
+class AspectRatioDemmo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Stack( //stack组件中，最大的子组件相当于底板
-            // alignment: Alignment.bottomLeft,
-            // alignment: Alignment.topRight,
-            alignment: Alignment.topLeft,
-            children: <Widget>[
-              SizedBox(
-              width: 200,
-              height: 300,
-              child: Container(
-                  // alignment: Alignment.bottomLeft,//相当于Alignment(-1.0,1.0)
-                  alignment: Alignment(0.0, -0.6),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(3, 54, 255, 1.0),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ))),
-          SizedBox(
-              width: 100,
-              height: 100,
-              child: Container(
-                  child: Icon(
-                    Icons.brightness_2,
-                    size: 32.0,
-                    color: Colors.white,
-                  ),
-                  decoration: BoxDecoration(
+    return AspectRatio(
+      // aspectRatio: 2.0/1.0,
+      // aspectRatio: 1.0/1.0, // 和aspectRatio:1 效果一致
+      // aspectRatio: 1.4,
+      aspectRatio: 16.0 / 9.0,
+      child: Container(
+        color: Color.fromRGBO(3, 54, 255, 1.0),
+      ),
+    );
+  }
+}
+
+class StackDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      //stack组件中，最大的子组件相当于底板
+      // alignment: Alignment.bottomLeft,
+      // alignment: Alignment.topRight,
+      alignment: Alignment.topLeft,
+      children: <Widget>[
+        SizedBox(
+            width: 200,
+            height: 300,
+            child: Container(
+                // alignment: Alignment.bottomLeft,//相当于Alignment(-1.0,1.0)
+                alignment: Alignment(0.0, -0.6),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(3, 54, 255, 1.0),
+                  borderRadius: BorderRadius.circular(8.0),
+                ))),
+        SizedBox(
+            width: 100,
+            height: 100,
+            child: Container(
+                child: Icon(
+                  Icons.brightness_2,
+                  size: 32.0,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
                     color: Color.fromRGBO(3, 54, 255, 1.0),
                     shape: BoxShape.circle,
-                    gradient: RadialGradient(
-                      colors: [
-                        Color.fromRGBO(7, 102, 255, 1.0),
-                        Color.fromRGBO(3, 54, 255, 1.0)
-                      ]
-                    )
-                  ))),
-                  Positioned( //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
-                    top: 20,
-                    right: 20,
-                    child:Icon(
-                    Icons.ac_unit,
-                    size: 16.0,
-                    color: Colors.white,
-                  )
-                  ),
-                  Positioned( //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
-                    top: 55,
-                    right: 45,
-                    child:Icon(
-                    Icons.ac_unit,
-                    size: 12.0,
-                    color: Colors.white,
-                  )
-                  ),
-                  Positioned( //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
-                    top: 90,
-                    right: 30,
-                    child:Icon(
-                    Icons.ac_unit,
-                    size: 16.0,
-                    color: Colors.white,
-                  )
-                  ),
-                  Positioned( //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
-                    top: 120,
-                    right: 5,
-                    child:Icon(
-                    Icons.ac_unit,
-                    size: 20.0,
-                    color: Colors.white,
-                  )
-                  ),
-                  Positioned( //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
-                    top: 160,
-                    right:60,
-                    child:Icon(
-                    Icons.ac_unit,
-                    size: 16.0,
-                    color: Colors.white,
-                  )
-                  ),
-                  Positioned( //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
-                    top: 200,
-                    right: 20,
-                    child:Icon(
-                    Icons.ac_unit,
-                    size: 28.0,
-                    color: Colors.white,
-                  )
-                  ),
-                  Positioned( //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
-                    bottom: 10,
-                    right: 10,
-                    child:Icon(
-                    Icons.ac_unit,
-                    size: 16.0,
-                    color: Colors.white,
-                  )
-                  )
-            ],
-          );
+                    gradient: RadialGradient(colors: [
+                      Color.fromRGBO(7, 102, 255, 1.0),
+                      Color.fromRGBO(3, 54, 255, 1.0)
+                    ])))),
+        Positioned(
+            //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
+            top: 20,
+            right: 20,
+            child: Icon(
+              Icons.ac_unit,
+              size: 16.0,
+              color: Colors.white,
+            )),
+        Positioned(
+            //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
+            top: 55,
+            right: 45,
+            child: Icon(
+              Icons.ac_unit,
+              size: 12.0,
+              color: Colors.white,
+            )),
+        Positioned(
+            //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
+            top: 90,
+            right: 30,
+            child: Icon(
+              Icons.ac_unit,
+              size: 16.0,
+              color: Colors.white,
+            )),
+        Positioned(
+            //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
+            top: 120,
+            right: 5,
+            child: Icon(
+              Icons.ac_unit,
+              size: 20.0,
+              color: Colors.white,
+            )),
+        Positioned(
+            //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
+            top: 160,
+            right: 60,
+            child: Icon(
+              Icons.ac_unit,
+              size: 16.0,
+              color: Colors.white,
+            )),
+        Positioned(
+            //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
+            top: 200,
+            right: 20,
+            child: Icon(
+              Icons.ac_unit,
+              size: 28.0,
+              color: Colors.white,
+            )),
+        Positioned(
+            //Positioned组件可以设置其top/left/right/bottomm属性，其child会相当于stack中最大的组件排列
+            bottom: 10,
+            right: 10,
+            child: Icon(
+              Icons.ac_unit,
+              size: 16.0,
+              color: Colors.white,
+            ))
+      ],
+    );
   }
 }
 
