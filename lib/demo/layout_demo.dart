628954d31beb1e9eaa5099974686a0aca563f419
ChenS-X <chensxyouxiang@163.com>
2020-07-09 06:24:42 +0800

    使用column和row组件，并设置其mainaxis和crossaxis

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -4,25 +4,39 @@ class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return IconBage(Icons.swap_calls);
+    return Container(
+      child: Column(
+      // child: Row(
+        // mainAxisAlignment: MainAxisAlignment.center, //默认是start
+        // mainAxisAlignment: MainAxisAlignment.end,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // crossAxisAlignment: CrossAxisAlignment.center, //默认是center
+        // crossAxisAlignment: CrossAxisAlignment.end,
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          IconBage(Icons.access_alarm),
+          IconBage(Icons.pool, size: 60.0,),
+          IconBage(Icons.beach_access)
+        ],
+      ),
+    );
   }
 }
-
-
 
 class IconBage extends StatelessWidget {
   final IconData icon;
   final double size;
 
-  IconBage(this.icon,{
-    this.size = 32.0
-  });
+  IconBage(this.icon, {this.size = 32.0});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      child: Icon(icon,size:size,color:Colors.white),
+      child: Icon(icon, size: size, color: Colors.white),
       width: size + 60,
       height: size + 60,
       color: Color.fromRGBO(3, 54, 255, 1.0),
