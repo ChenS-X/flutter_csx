3d9556357fae9fe10296575d80fe767e66b7b14d
ChenS-X <chensxyouxiang@163.com>
2020-07-15 07:19:14 +0800

    使用sliversafearea和sliverpadding优化视图

--- lib/demo/silver_demo.dart
+++ lib/demo/silver_demo.dart
@@ -2,43 +2,41 @@ import 'package:flutter/material.dart';
 
 import '../modle/post.dart';
 
-class SilverDemo extends StatelessWidget{
+class SilverDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          SilverGridDemo()
+          SliverSafeArea(
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SilverGridDemo(),
+            ),
+          )
         ],
       ),
     );
   }
 }
 
-
 class SilverGridDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
-              childAspectRatio: 0.7 //定义网格的宽高比，默认是1.0
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index){
-                return Container(
-                  child: Image.network(
-                    posts[index].imageUrl,
-                    fit:BoxFit.cover
-                  ),
-                );
-              },
-              childCount: posts.length
-            ),
-          );
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+          childAspectRatio: 0.7 //定义网格的宽高比，默认是1.0
+          ),
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        return Container(
+          child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+        );
+      }, childCount: posts.length),
+    );
   }
 }
