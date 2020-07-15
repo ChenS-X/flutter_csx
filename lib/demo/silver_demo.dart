613e3d9845fe6a7e0e44d638a1483598ae3d780d
ChenS-X <chensxyouxiang@163.com>
2020-07-16 07:09:35 +0800

    使用sliverappbar

--- lib/demo/silver_demo.dart
+++ lib/demo/silver_demo.dart
@@ -9,6 +9,21 @@ class SilverDemo extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
+          SliverAppBar(
+            // title: Text('Nihao'),
+            // pinned: true, //默认是false，设置为true，表示固定appbar，不让其滚动隐藏
+            floating: true,
+            expandedHeight: 178.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('nihao,haha'.toUpperCase(),style: TextStyle(
+                letterSpacing: 5.0,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),),
+              centerTitle: true,
+              background: Image.network('https://cn.bing.com/th?id=OHR.FrederickSound_ZH-CN1838908749_1920x1080.jpg&rf=LaDigue_1920x1080.jpg',fit:BoxFit.cover),
+            ),
+          ),
           SliverSafeArea(
             sliver: SliverPadding(
               padding: EdgeInsets.all(8.0),
